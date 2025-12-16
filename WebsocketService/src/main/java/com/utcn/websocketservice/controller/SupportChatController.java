5098665270d7c5fe2860ce721ba7cdd61ae836d5
GeorgeTof <tofangeorge23@gmail.com>
2025-12-17 00:42:11 +0200

    extend default support chat options

--- WebsocketService/src/main/java/com/utcn/websocketservice/controller/SupportChatController.java
+++ WebsocketService/src/main/java/com/utcn/websocketservice/controller/SupportChatController.java
@@ -25,8 +25,34 @@ public class SupportChatController {
 
         if (content.contains("total") && content.contains("consumption")) {
             responseText = "To view your consumption, navigate to the Dashboard and click 'Show My Energy Consumption'.";
-        } else if (content.contains("device")) {
+        }
+        else if (content.contains("total") && content.contains("devices")) {
             responseText = "You can view your associated devices in the 'My Devices' list on the dashboard.";
+        }
+        else if (((content.contains("specific") || content.contains("individual")) && content.contains("device"))
+                && content.contains("consumption")) {
+            responseText = "To view a specific device's consumption, find the device card in your list and click the specific 'Consumption' button on that card (do not use the main button).";
+        }
+        else if (content.contains("change") && content.contains("password")) {
+            responseText = "Changing your password is not currently supported. Please keep your credentials safe.";
+        }
+        else if (content.contains("delete") && content.contains("device")) {
+            responseText = "Only administrators can delete devices. Please contact an admin.";
+        }
+        else if (content.contains("overconsumption")) {
+            responseText = "If overconsumption occurs, you will receive an automatic warning alert. Please contact an administrator immediately if this happens repeatedly.";
+        }
+        else if (content.contains("delete") && content.contains("account")) {
+            responseText = "To delete your account, please contact an administrator.";
+        }
+        else if (content.contains("view") && content.contains("today")) {
+            responseText = "To view today's data, click 'Show My Energy Consumption' and simply select today's date in the calendar popup.";
+        }
+        else if (content.contains("update") && content.contains("email")) {
+            responseText = "To update your email address, please contact an administrator.";
+        }
+        else if (content.contains("pay") || content.contains("payment") || content.contains("subscription")) {
+            responseText = "Our application is free to use, but donations are appreciated!";
         }
 
         ChatMessage response = new ChatMessage("Support Bot", responseText);
